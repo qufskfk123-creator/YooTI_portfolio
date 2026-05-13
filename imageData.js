9fa2000646741eeee279653fdd7a6b2f9d7d8ec3
qufskfk123-creator <qufskfk123@gmail.com>
2026-05-13 16:34:34 +0900

    Add files via upload

--- imageData.js
+++ imageData.js
@@ -3,23 +3,31 @@
 // 고해상도: true면 lightbox에서 -lg.확장자 사용 (3840px 원본)
 var imageDataList = [
   ["hero-bg", "Hero Background", "", "", "webm"],
-  ["profile", "About Me", "3D Graphics & Technical Designer", "", "webp"],
+  ["profile", "About Me", "3D Graphics & Technical Designer", "", "jpg"],
   ["1", "Omniverse", "IsaacSIM Learning test", "", "mp4"],
-  ["2", "Project 2", "test2", "", "webp"],
-  ["3", "Project 3", "test3", "", "webp"],
-  ["4", "Project 4", "Description", "", "webp"],
-  ["5", "Project 5", "Description", "", "webp"],
-  ["6", "Project 6", "Description", "", "webp"],
-  ["7", "Project 7", "Description", "", "webp"],
-  ["8", "Project 8", "Description", "", "webp"],
-  ["9", "Project 9", "Description", "", "webp"],
+  ["2", "Industrial Park", "Industrial Project", "", "jpg"],
+  ["3", "Project 3", "test3", "", "jpg"],
+  ["4", "Project 4", "Description", "", "jpg"],
+  ["5", "Project 5", "Description", "", "jpg"],
+  ["6", "Project 6", "Description", "", "jpg"],
+  ["7", "Project 7", "Description", "", "jpg"],
+  ["8", "Project 8", "Description", "", "jpg"],
+  ["9", "Project 9", "Description", "", "jpg"],
 ];
 
 // Image 탭 추가 이미지
 // 형식: ["키", "제목", "설명", "링크URL", "확장자", "고해상도원본유무"],
 var imageGalleryExtra = [
-  ["1-1", "Omniverse detail 1", "", "", "webp", true],
-  ["1-2", "Omniverse detail 2", "", "", "webp", true],
-  ["1-3", "Omniverse detail 3", "", "", "webp", true],
-  ["2-1", "Project 2 detail", "", "", "webp"],
+  ["1-1", "Omniverse detail 1", "", "", "jpg", false],
+  ["1-2", "Omniverse detail 2", "", "", "jpg", false],
+  ["1-3", "Omniverse detail 3", "", "", "jpg", false],
+  ["1-4", "Omniverse detail 4", "", "", "jpg", false],
+  ["1-5", "Omniverse detail 5", "", "", "jpg", false],
+  ["2-1", "Project 1 detail", "", "", "jpg"],
+  ["2-2", "Project 2 detail", "", "", "jpg"],
+  ["2-3", "Project 3 detail", "", "", "jpg"],
+  ["2-4", "Project 4 detail", "", "", "jpg"],
+  ["2-5", "Project 5 detail", "", "", "jpg"],
+  ["2-6", "Project 6 detail", "", "", "jpg"],
+
 ];
